85b243a928dceb00685f04868d02fd66b2277ffa
Sneha Naphade <sneha.naphade833@gmail.com>
2022-03-07 22:30:05 +0530

    update

--- script.js
+++ script.js
@@ -26,3 +26,21 @@ darkmode.onclick = () => {
     }
 
 }
+const swiper = new Swiper('.swiper', {
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  
+    // And if we need scrollbar
+    
+  });
